7bce6550af6ad6e2964d6d95220d3310e0f6f54c
thegeezer3 <adamsubscribe@googlemail.com>
2011-02-01 13:20:07 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robodisco3::Application.config.secret_token = '5c75da7254490a2db18357a64a33fdde561e5f14ce6298f4c916ab31a7ebc02afbccc426bb66546898af7674932b5247660893386b4e9c985d05e4de5178946c'
