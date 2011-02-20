d2b2610ae98ffd11faaa12e2ce02a8569864060b
thegeezer3 <adamsubscribe@googlemail.com>
2011-02-20 23:01:58 +0900

    enable ajax favourite toggling

--- app/helpers/player_helper.rb
+++ app/helpers/player_helper.rb
@@ -2,12 +2,12 @@ module PlayerHelper
   def favourite_link
     if user_signed_in?
       if current_user.albums.find_by_mb_id(@album.mb_id)
-        button_to "Remove From Favourites", toggle_favourite_user_album_path(current_user, @album),{ :class => "favourite" }
+        button_to "Unfavourite", toggle_favourite_user_album_path(current_user, @album),{ :class => "favourite_toggle", :id => @album.id }
       else
-        button_to "Add To Favourites", toggle_favourite_user_album_path(current_user, @album), { :class => "favourite" }
+        button_to "Favourite", toggle_favourite_user_album_path(current_user, @album), { :class => "favourite_toggle", :id => @album.id }
       end
     else
-        button_to "Add To Favourites", "#", :class => "favourite"
+        button_to "Favourite", "#", :class => "login_required"
     end
   end
 end
