93801a2f6a221d6243dc2af2fc05728d03b70d66
thegeezer3 <adamsubscribe@googlemail.com>
2011-02-24 14:42:51 +0900

    add user to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+User.create(:email => "adamsubscribe@googlemail.com", :password => "secrets", :password_confirmation => "secrets");
