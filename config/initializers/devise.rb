a300a59b66d20edc095a0b37ff1e586eb609eb39
thegeezer3 <adamsubscribe@googlemail.com>
2011-03-15 20:07:01 +0900

    expire account invite after 1 day

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -51,7 +51,7 @@ Devise.setup do |config|
   # ==> Configuration for :invitable
   # Time interval where the invitation token is valid (default: 0).
   # If invite_for is 0 or nil, the invitation will never expire.
-  # config.invite_for = 2.weeks
+   config.invite_for = 1.days
   
   # ==> Configuration for :confirmable
   # The time you want to give your user to confirm his account. During this time
