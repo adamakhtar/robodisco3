adabfc18005fce35787ee3990afef2efb084e36e
thegeezer3 <adamsubscribe@googlemail.com>
2011-03-30 08:02:38 +0900

    create flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
-
-
+  def flash_messages(flash)
+    content_tag :div, :id => "flash" do
+      [:notice, :alert, :errors].map do |type|
+        content_tag :p, flash[type], :class => type.to_s if flash[type]
+      end.join.html_safe
+    end
+  end
 end
