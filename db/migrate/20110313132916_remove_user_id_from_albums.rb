45f4faf3200d734ade2f1ec47f0da7369e216a40
thegeezer3 <adamsubscribe@googlemail.com>
2011-04-01 00:02:27 +0900

    remove duplicate migration

--- db/migrate/20110313132916_remove_user_id_from_albums.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserIdFromAlbums < ActiveRecord::Migration
-  def self.up
-    remove_column :albums, :user_id
-  end
-
-  def self.down
-    add_column :albums, :user_id, :integer
-  end
-end
