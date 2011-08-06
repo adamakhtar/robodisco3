554ad888daa241993c71e4b8ada0b450a16a71cc
robodisco <adamsubscribe@googlemail.com>
2011-08-06 20:07:21 +0900

    refactor player spec

--- spec/requests/player_stories_spec.rb
+++ spec/requests/player_stories_spec.rb
@@ -1,16 +1,26 @@
 require 'spec_helper'
 
-describe "PlayerStories" do
-  describe "when user selects album" do
+describe "Player" do
+  context "when user selects album" do
     use_vcr_cassette("play_album_thriller_cassette")
-    it "shows all the albums tracks" do
+    before(:each) do
       thriller_album_id = "959272f9-97ae-4179-aebe-950eef64ed93"
       log_in
       visit play_album_path(:mb_id => thriller_album_id)
       current_path.should match /#{play_album_path}/
-
+    end
+    
+    it "shows all the albums tracks" do
       page.should have_selector("#track_listing")
       page.find("#track_listing").should have_selector("li")
     end
+
+
+    it "shows other users recent albums" do
+      page.should have_selector("#related_albums")
+      page.find("#related_albums").should have_selector("li")
+    end
   end
+
+
 end
