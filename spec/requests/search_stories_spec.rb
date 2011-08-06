2895457ed6b78735d2e94f871a20e59b11d6602f
robodisco <adamsubscribe@googlemail.com>
2011-08-06 14:04:39 +0900

    refactor search stories spec to use log in helper

--- spec/requests/search_stories_spec.rb
+++ spec/requests/search_stories_spec.rb
@@ -3,15 +3,7 @@ require 'spec_helper'
 describe "SearchStories" do
   describe "when user enters valid search on homepage" do
     it "dispalys search results on search page" do
-      user = Factory(:user)
-      visit "/"
-      page.click_link("sign_in")
-      within("form#user_new") do
-        fill_in("user_username", :with => user.username)
-        fill_in("user_password", :with => user.password)
-        click_button("user_submit")
-      end
-      
+      log_in     
       within("form#search_form") do
         fill_in("query", :with => "michael jackson")
         click_button("search_submit")
@@ -24,14 +16,7 @@ describe "SearchStories" do
 
   describe "when user enters invalid search from homepage" do
     it "dispalys no results found on search page" do
-      user = Factory(:user)
-      visit "/"
-      page.click_link("sign_in")
-      within("form#user_new") do
-        fill_in("user_username", :with => user.username)
-        fill_in("user_password", :with => user.password)
-        click_button("user_submit")
-      end
+      log_in
       
       within("form#search_form") do
         fill_in("query", :with => ";;;..,,,")
