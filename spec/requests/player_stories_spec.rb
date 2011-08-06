67720ed58125fc64e328143f2c2f6393703cb2a1
robodisco <adamsubscribe@googlemail.com>
2011-08-07 00:42:45 +0900

    test correct album is loaded in player

--- spec/requests/player_stories_spec.rb
+++ spec/requests/player_stories_spec.rb
@@ -9,13 +9,18 @@ describe "Player" do
       visit play_album_path(:mb_id => thriller_album_id)
       current_path.should match /#{play_album_path}/
     end
+
+    it "loads the album in the player" do
+      page.find(".album_name").should have_content("Thriller")
+    end
+
     
     it "shows all the albums tracks" do
       page.should have_selector("#track_listing")
       page.find("#track_listing").should have_selector("li")
     end
 
-    
+
     it "shows other users recent albums", :js => true do 
       page.should have_selector("#related_albums")
       page.find("#related_albums").should have_selector("li")
